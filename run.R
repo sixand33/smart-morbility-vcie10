23f35b75f3dfe84c0b44c2a603ae7c9c8c041395
latin-rock-developer <65062147+sixand33@users.noreply.github.com>
2021-09-22 09:53:47 -0500

    Add files via upload

--- run.R
+++ run.R
@@ -1,9 +1,10 @@
 library(shiny)
+library(rstudioapi)
 
 port <- Sys.getenv('PORT')
 
 shiny::runApp(
-  appDir = getwd(),
+  appDir = dirname(rstudioapi::getSourceEditorContext()$path),
   host = '0.0.0.0',
   port = as.numeric(port)
 )
